02f2943a0899377199ec1c6249a040d9acf57cb4
Jeremy Pritts <jrpritts7@gmail.com>
2021-11-18 21:42:14 -0500

    fix bugs

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -2,37 +2,19 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Fire-Pack")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Fire-Pack")]
-[assembly: AssemblyCopyright("© 2021 WulfMarius, ds5678")]
+[assembly: AssemblyCopyright("© 2021 ds5678")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("14d19c5c-bd92-49d7-8c69-b46c78b7cdcf")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.0")]
-[assembly: AssemblyFileVersion("2.5.0")]
-[assembly: MelonInfo(typeof(FirePack.Implementation), "Fire-Pack", "2.5.0", "ds5678")]
+[assembly: AssemblyVersion("2.5.1")]
+[assembly: AssemblyFileVersion("2.5.1")]
+[assembly: MelonInfo(typeof(FirePack.Implementation), "Fire-Pack", "2.5.1", "ds5678")]
 [assembly: MelonGame("Hinterland", "TheLongDark")]
